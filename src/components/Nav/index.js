d2117e799f2e8eaa617507594afe6313f2d11371
TiphaineLeloup <tiphaineleloup@orange.fr>
2019-07-25 10:29:28 +0200

    test sur push

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,8 +5,7 @@ import './nav.scss';
 const Nav = () => (
   <ul className="side-menu">
     <li className="menu-item"><a href="#" className="menu-item-link">Map</a></li>
-    <li className="menu-item"><a href="#" className="menu-item-link">Mes groupes</a></li>
-    <li className="menu-item"><a href="#" className="menu-item-link">Mes évènements</a></li>
+    <li className="menu-item"><a href="#" className="menu-item-link">Mon tableau de bord</a></li>
     <li className="menu-item"><a href="#" className="menu-item-link">Mon profil</a></li>
     <li className="menu-item"><a href="#" className="menu-item-link">Contactez-nous</a></li>
   </ul>
