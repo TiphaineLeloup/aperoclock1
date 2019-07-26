f44da5af98365f10c31d90fa6bff148f8c2708f8
Valtalyn <valtalyn@velmys.home>
2019-07-26 12:14:24 +0200

    Corrections TextArea

--- src/components/EventForm/index.js
+++ src/components/EventForm/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Input } from 'antd';
-import { TextArea } from 'antd';
 import { Button } from 'antd';
 import './eventform.scss';
 
 
-const EventForm = () => (
-  <form>
-    <Input placeholder="Nom de l'évènement" />;
-    <TextArea placeholder="Description de l'évènement" rows={4} />;
-    <Button type="link">Valider</Button>
-  </form>
-);
+const EventForm = () => {
+  const { TextArea } = Input;
+
+  return (
+    <form>
+      <Input placeholder="Nom de l'évènement" />;
+      <TextArea placeholder="Description de l'évènement" rows={4} />;
+      <Button type="link">Valider</Button>
+    </form>
+  );
+};
 
 export default EventForm;
--- src/components/GroupForm/index.js
+++ src/components/GroupForm/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Input } from 'antd';
-import { TextArea } from 'antd';
 import { Button } from 'antd';
 import './groupform.scss';
 
 
-const GroupForm = () => (
-  <form>
-    <Input placeholder="Nom du groupe" />;
-    <TextArea placeholder="Description du groupe" rows={4} />;
-    <Button type="link">Valider</Button>
-  </form>
-);
+const GroupForm = () => {
+  const { TextArea } = Input;
+  return (
+    <form>
+      <Input placeholder="Nom du groupe" />;
+      <TextArea placeholder="Description du groupe" rows={4} />;
+      <Button type="link">Valider</Button>
+    </form>
+  );
+};
 
 export default GroupForm;
